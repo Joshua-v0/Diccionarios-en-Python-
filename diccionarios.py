84e110373475197dee65a06e403f50550a93d281
Joshua Voitier <126485938+Joshua-v0@users.noreply.github.com>
2023-10-21 13:28:05 -0500

    Add files via upload

---
+++ diccionarios.py
@@ -0,0 +1,22 @@
+"""diccionario = {"Alemania": "Berlin","Colombia":"bogota","Panama":"Panama"}
+
+#como agregar un elemento a un diccionaro
+diccionario["EU"] = "Washintong"
+print(diccionario)
+
+#como cambiar un dato de un diccionario
+diccionario["EU"] = "Texas"
+print(diccionario) 
+
+#como eliminar un diccionario
+del diccionario["Alemania"]
+print(diccionario)"""
+
+#Diccionarios con Strings 
+"""diccionario = {21: "joshua", "Jason":25} 
+print(diccionario[21], "tiene", +  diccionario["Jason"])"""
+
+
+   
+
+  
